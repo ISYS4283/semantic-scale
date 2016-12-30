e96d29a20fc5720a96fb8f0f20f6f129a17e6acf
Jeff Puckett <jeff@jeffpuckett.com>
2016-12-30 22:54:33 +0000

    add bootstrap progress bar

--- src/SemanticScale.php
+++ src/SemanticScale.php
@@ -1,5 +1,7 @@
 <?php namespace jpuck\wordpress\plugins\SemanticScale;
 
+use jpuck\php\bootstrap\ProgressBar\ProgressBar;
+
 class SemanticScale {
 	private static $instance;
 
@@ -11,15 +13,17 @@ class SemanticScale {
 		// TODO: use configuration manager
 		$this->wordsource = new WordSourceJsonFile(__DIR__.'/../scratch.json');
 
+		add_action( 'wp_head',     array( ProgressBar::class, 'wp_head' ) );
 		add_filter( 'the_content', array( $this, 'the_content' ) );
 		add_action( 'save_post',   array( $this, 'save_post' ), 1, 3 );
 	}
 
 	public function the_content($input) {
-		$grade = get_post_meta( get_the_ID(), 'semantic-scale', true );
-		// TODO: handle if empty
-		// TODO: add output class
-		return $input . "<p>Semantic Scale: $grade</p>";
+		$grade = (int) get_post_meta( get_the_ID(), 'semantic-scale', true );
+
+		$progressbar = new ProgressBar($grade);
+
+		return $input . "<hr/><p><strong>Semantic Scale:</strong>$progressbar</p>";
 	}
 
 	public function save_post(int $post_ID, \WP_Post $post, bool $update) {
