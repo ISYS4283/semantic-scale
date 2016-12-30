a51b24bbb261611a56ab592a66bbbb08ca89749c
Jeff Puckett <jeff@jeffpuckett.com>
2016-12-30 00:38:17 +0000

    update_post_meta with last modified datetime

--- src/SemanticScale.php
+++ src/SemanticScale.php
@@ -13,9 +13,15 @@ class SemanticScale {
 
 	public function __construct() {
 		add_filter( 'the_content', array( $this, 'the_content' ) );
+		add_action( 'save_post',   array( $this, 'save_post' ), 1, 3 );
 	}
 
 	public function the_content($input) {
-		return $input . "<p>This is the Semantic Scale plugin.</p>";
+		$content = get_post_meta( get_the_ID(), 'semantic-scale-mod', true );
+		return $input . "<p>Semantic Scale last modified: $content</p>";
+	}
+
+	public function save_post(int $post_ID, \WP_Post $post, bool $update) {
+		update_post_meta( $post_ID, 'semantic-scale-mod', gmdate("Y-m-d H:i:s") );
 	}
 }
