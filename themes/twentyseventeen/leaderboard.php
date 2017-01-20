237eb598e40d86a9864e39ea7466591e0944959e
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-20 21:27:06 +0000

    add stub template for leaderboard

---
+++ themes/twentyseventeen/leaderboard.php
@@ -0,0 +1,43 @@
+<?php
+ /*
+  Template Name: Semantic Scale Leaderboard
+ */
+ /**
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header();
+the_post();
+?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+
+			<style>
+				#comments {
+					float: none !important;
+					width: 100% !important;
+				}
+			</style>
+			<?php
+			the_content();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
+
+		</main>
+	</div>
+</div>
+
+<?php get_footer();
